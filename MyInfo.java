6fb1ca0edc401a34d2940754eb12c7ce7c69a067
Llewil Aeneid Montilla <codelouie94@gmail.com>
2022-02-07 17:00:48 -0800

    first commit

---
+++ MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Louie Montilla");
+        System.out.println("I am 27 years old");
+        System.out.println("My hometown is Cebu, Philippines");
+    }
+}
